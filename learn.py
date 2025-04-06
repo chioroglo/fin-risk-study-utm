091030800e5f498f73b31622e9be900d2eb75456
chioroglo <kioroglomobster@gmail.com>
2025-04-06 22:29:19 +0300

    fix model target variable & saving location

--- learn.py
+++ learn.py
@@ -1,3 +1,4 @@
+import os
 import joblib
 import pandas as pd
 import numpy as np
@@ -5,8 +6,8 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
+from pathlib import Path
 
-from analyse_model import analyse_model
 from target_mean_encoding import target_mean_encoding
 
 
@@ -27,20 +28,20 @@ df['AI_Used_for_Auditing'] = df['AI_Used_for_Auditing'].map({'Yes': 1, 'No': 0})
 # Apply Target Mean Encoding to categorical columns
 for col in ['Industry_Affected']:
     # Calculate target mean for each category
-    mean_encoded = df.groupby(col)['Client_Satisfaction_Score'].mean()
+    mean_encoded = df.groupby(col)['Audit_Effectiveness_Score'].mean()
     
     # Save the means to a CSV file
     mean_encoded.to_csv(f'{col}_target_means.csv', header=True)
     
     # Apply target mean encoding to the dataset
-    df[f'{col}_Encoded'] = target_mean_encoding(df, col, 'Client_Satisfaction_Score')
+    df[f'{col}_Encoded'] = target_mean_encoding(df, col, 'Audit_Effectiveness_Score')
 
 # Drop original categorical columns after encoding
 df = df.drop(['Industry_Affected'], axis=1)
 
 # Separate features and target variable
-X = df.drop('Client_Satisfaction_Score', axis=1)
-y = df['Client_Satisfaction_Score']
+X = df.drop('Audit_Effectiveness_Score', axis=1)
+y = df['Audit_Effectiveness_Score']
 
 # Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
@@ -63,8 +64,9 @@ print(f'Mean Squared Error: {mse}')
 print(f'R-squared: {r2}')
 
 # Save model and scaler
-joblib.dump(model, "model.pkl")
-joblib.dump(scaler, "scaler.pkl")
+Path("resources").mkdir(parents=True,exist_ok=True)
+joblib.dump(model, "resources/model.pkl")
+joblib.dump(scaler, "resources/scaler.pkl")
 
 
 # analysis
